af93d46cd121111c42f2b05300cdf120ab366623
jmolina91 <javier.m@thoughtworks.com>
2021-02-27 23:36:37 +0100

    Reloading code

--- api/app/Http/Controllers/UsersController.php
+++ api/app/Http/Controllers/UsersController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers;
 
-class ExampleController extends Controller
+class UsersController extends Controller
 {
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function index()
     {
-        //
+        return "Hello From Index";
     }
 
     //

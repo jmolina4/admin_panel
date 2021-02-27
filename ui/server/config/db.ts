660732677953c01053d3c8f91c65e2e4882e1803
jmolina91 <javier.m@thoughtworks.com>
2021-02-27 18:54:54 +0100

    Basic vue app

--- ui/server/config/db.ts
+++
@@ -1,8 +0,0 @@
-import { Client, config } from '../deps.ts';
-
-const client: any = new Client();
-client.setEndpoint(config().APP_WRITE_API_ENDPOINT);
-client.setKey(config().APP_WRITE_API_KEY);
-client.setProject(config().APP_WRITE_PROJECT_ID);
-
-export default client;
